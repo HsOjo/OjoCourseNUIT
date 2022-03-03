4bebfa43089746624e58b869910d9758fe089b2b
HsOjo <1134031392@qq.com>
2022-03-03 18:08:56 +0800

    [fix] 修复信息获取问题

--- app/api/info/service.py
+++ app/api/info/service.py
@@ -18,14 +18,33 @@ class CourseService(Service):
         return InfoService()
 
     def get_info(self, user: User):
-        info = self.service_info.course_info(user.info.class_)
+        try:
+            info = self.service_info.course_info(user.info.class_)
+        except AccessError:
+            info = None
+
+        course = self.filters(user_id=user.id).get_one()  # type: Course
         if info:
-            self.filters(user_id=user.id).without_pk_filter.edit(data=info)
+            if course:
+                self.edit(course.id, data=info)
+            else:
+                self.add(user_id=user.id, data=info)
         else:
-            course = self.filters(user_id=user.id).get_one()  # type: Course
-            info = course.data
+            if course:
+                info = course.data
 
         return info
+
+
+def handle_access_err(f):
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except AccessError as e:
+            redis.cli.delete(RK_COOKIES)
+            raise e
+
+    return wrapper
 
 
 class InfoService:
@@ -40,9 +59,11 @@ class InfoService:
 
         return NUIT(cfg.url_base, cookies)
 
+    @handle_access_err
     def basic_info(self):
         return self.nuit.get_basic_info()
 
+    @handle_access_err
     def college_info(self):
         result = self.basic_info()
         colleges = {k: v for k, v in result.pop('colleges').items() if v}
@@ -52,31 +73,31 @@ class InfoService:
             grades=grades,
         )
 
+    @handle_access_err
     def semester_info(self):
         result = self.basic_info()
         result.pop('colleges')
         result['dates'] = self.date_info(result.get('stu_year'))
         return result
 
+    @handle_access_err
     def major_info(self, college):
         result = self.nuit.get_major_info(college)
         return result
 
+    @handle_access_err
     def class_info(self, college, major, grade):
         result = self.nuit.get_class_info(college, major, grade)
         return result
 
+    @handle_access_err
     def course_info(self, class_, week=''):
-        try:
-            basic_info = self.basic_info()
-            stu_year = basic_info['stu_year']
-            result = self.nuit.get_course_info(class_, stu_year, week)
-        except AccessError:
-            redis.cli.delete(RK_COOKIES)
-            result = None
-
+        basic_info = self.basic_info()
+        stu_year = basic_info['stu_year']
+        result = self.nuit.get_course_info(class_, stu_year, week)
         return result
 
+    @handle_access_err
     def date_info(self, stu_year):
         basic_info = self.basic_info()
         result = self.nuit.get_date_info(stu_year, basic_info['week_total'])
